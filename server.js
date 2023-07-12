396acc6d762163f9c0ee29f1e66426b62d3ddf0c
ImadeddineBouzair <bouzairimad@gmail.com>
2023-07-11 18:17:29 -0700

    ..

--- server.js
+++ server.js
@@ -1,22 +1,15 @@
 const express = require('express');
+const multer = require('multer');
+const upload = multer();
 const appRoutes = require('./routes/index');
-const cloudinary = require('cloudinary').v2;
 
 require('dotenv').config();
 require('./config/database').connect();
 
-// Cloudinary configuration
-cloudinary.config({
-  cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
-  api_key: process.env.CLOUDINARY_API_KEY,
-  api_secret: process.env.CLOUDINARY_API_SECRET,
-  secure: true,
-});
-console.log(cloudinary.config());
-
 const app = express();
 
 app.use(express.json());
+app.use(upload.array());
 app.use('/api/v1/shop', appRoutes);
 
 app.listen(process.env.PORT, () =>
